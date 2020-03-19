440fe37120b9c3add586d08258da130bf241315b
Dev <dev234tripathi@gmail.com>
2020-03-19 10:09:43 +0530

    Add files via upload

---
+++ BinarySearch/Two Sum II - Input array is sorted.java
@@ -0,0 +1,34 @@
+/**
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+Note:
+
+Your returned answers (both index1 and index2) are not zero-based.
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+Example:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+*/
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        
+        for(int i=0;i<numbers.length;i++) {
+            int start = i+1, end = numbers.length-1;
+            int temp = target - numbers[i];
+            while(start <= end) {
+                int mid = start + (end-start)/2;
+                if(numbers[mid] == temp)
+                    return new int[]{i+1,mid+1};
+                else if(numbers[mid] < temp)
+                    start = mid+1;
+                else
+                    end = mid-1;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
